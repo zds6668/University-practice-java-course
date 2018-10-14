f8135cb96abf1d6ddf37581296a41334881f9423
dqw6668 <yjdqw@126.com>
2018-10-14 22:21:01 +0800

    14-9

---
+++ thread/src/ThreadGate.java
@@ -0,0 +1,24 @@
+/**
+ * 14-9
+ */
+public class ThreadGate {
+    private boolean isOpen;
+    private int generation;
+
+    public synchronized void close() {
+        isOpen = false;
+    }
+
+    public synchronized void open() {
+        ++generation;
+        isOpen = true;
+        notifyAll();
+    }
+
+    //阻塞直到opened-since(generation on entry)
+    public synchronized void await() throws InterruptedException {
+        int arrivalGeneration = generation;
+        while (!isOpen && arrivalGeneration == generation)
+            wait();
+    }
+}
